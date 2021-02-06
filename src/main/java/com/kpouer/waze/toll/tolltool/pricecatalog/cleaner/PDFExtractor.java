93de7a4354902cc843f30a6a5f779e24149c78ba
Matthieu Casanova <chocolat.mou@gmail.com>
2021-02-06 14:21:04 +0100

    add a pdf extractor

---
+++ src/main/java/com/kpouer/waze/toll/tolltool/pricecatalog/cleaner/PDFExtractor.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Matthieu Casanova
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ *  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ *  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.kpouer.waze.toll.tolltool.pricecatalog.cleaner;
+
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PDFExtractor {
+    public static String extractText(File pdfFile) throws IOException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(pdfFile, "r");
+        PDFParser        parser           = new PDFParser(randomAccessFile);
+        parser.parse();
+        PDDocument      pdDocument  = parser.getPDDocument();
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        String text = pdfStripper.getText(pdDocument);
+        pdDocument.close();
+        return text;
+    }
+}
