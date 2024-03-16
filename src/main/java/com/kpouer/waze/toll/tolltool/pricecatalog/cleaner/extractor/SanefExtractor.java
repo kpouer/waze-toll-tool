a72b74348ecfbe51b24f9aa43c9995046a6e0300
Matthieu Casanova <public@kpouer.com>
2024-03-16 18:16:46 +0100

    do sanef export

--- src/main/java/com/kpouer/waze/toll/tolltool/pricecatalog/cleaner/extractor/SanefExtractor.java
+++ src/main/java/com/kpouer/waze/toll/tolltool/pricecatalog/cleaner/extractor/SanefExtractor.java
@@ -21,31 +21,28 @@
  */
 package com.kpouer.waze.toll.tolltool.pricecatalog.cleaner.extractor;
 
+import com.kpouer.waze.toll.tolltool.pricecatalog.Category;
 import com.kpouer.waze.toll.tolltool.pricecatalog.cleaner.PDFExtractor;
 import com.kpouer.waze.toll.tolltool.pricecatalog.cleaner.builder.TriangleBuilder;
 import lombok.RequiredArgsConstructor;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Year;
-import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.time.LocalDate;
 import java.util.regex.Pattern;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 @RequiredArgsConstructor
 public class SanefExtractor implements Extractor {
-    private static final Pattern EURO    = Pattern.compile(" €");
-    private static final Pattern MINUS   = Pattern.compile("(A\\d+) - ");
-    private static final Pattern TABS    = Pattern.compile("(\\d) (\\d)");
-    private static final Pattern TABS2   = Pattern.compile("(\\d) ([A-Z])");
-    private static final Pattern TABS3   = Pattern.compile("([A-Z]) (A\\d)");
-    private static final Pattern TABS4   = Pattern.compile("(\\)) (\\d)");
-    private static final Pattern TABS5   = Pattern.compile("(\\)) (A\\d)");
-    private static final Pattern TABS6   = Pattern.compile("([A-Z]) (\\d)");
-    private static final Pattern SPACE   = Pattern.compile(" ");
+    private static final Pattern EURO = Pattern.compile(" €");
+    private static final Pattern MINUS = Pattern.compile("(A\\d+) - ");
+    private static final Pattern TABS = Pattern.compile("(\\d) (\\d)");
+    private static final Pattern TABS2 = Pattern.compile("(\\d) ([A-Z])");
+    private static final Pattern TABS3 = Pattern.compile("([A-Z]) (A\\d)");
+    private static final Pattern TABS4 = Pattern.compile("(\\)) (\\d)");
+    private static final Pattern TABS5 = Pattern.compile("(\\)) (A\\d)");
+    private static final Pattern TABS6 = Pattern.compile("([A-Z]) (\\d)");
+    private static final Pattern SPACE = Pattern.compile(" ");
     private static final Pattern COMPILE = Pattern.compile(" (\\d)");
 
     private final Path pdf;
@@ -54,103 +51,49 @@ public class SanefExtractor implements Extractor {
     public void extract() throws IOException {
         var outputPath = Path.of(pdf.getParent().toString(), "out");
         var triangleOutputPath = Path.of(outputPath.toString(), "triangle");
-        var triangleBuilder  = new TriangleBuilder(triangleOutputPath);
-        String text     = PDFExtractor.extractText(pdf.toFile());
-        text = text.replaceAll("\r", "");
-        String[] lines       = text.split("\n");
-        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(Path.of(outputPath.toString(), Year.now() + "_SANEF.tsv"), UTF_8))) {
-            AtomicInteger errors = new AtomicInteger();
-            Arrays.stream(lines)
-                  .filter(line -> !line.contains("VEHICULES"))
-                  .filter(line -> !line.contains("tarifs"))
-                  .filter(line -> !line.contains("véhicules"))
-                  .filter(line -> !line.contains("Hauteur"))
-                  .filter(line -> !line.contains("inférieur"))
-                  .filter(line -> !line.contains("CLASSE"))
-                  .filter(line -> !line.contains("égal"))
-                  .map(line -> EURO.matcher(line).replaceAll(""))
-                  .map(line -> SPACE.matcher(line).replaceAll("\t"))
-                  .map(line -> line.replaceAll("\tN°", " N°"))
-                  .map(line -> line.replaceAll("([A-Z])\t([A-Z])", "$1 $2"))
-                  .map(line -> line.replaceAll("19\tA4", "19 A4"))
-                  .map(line -> line.replaceAll("13\tA29", "13 A29"))
-                  .map(line -> line.replaceAll("e\t", "e "))
-                  .map(line -> line.replaceAll("33\tA", "33 A"))
-                  .map(line -> line.replaceAll("\t\\(", " ("))
-                  .map(line -> line.replaceAll("/\t", "/ "))
-                  .map(line -> line.replaceAll("\t/", " /"))
-                  .map(line -> line.replaceAll(" ST\t", " ST "))
-                  .map(line -> line.replaceAll("\tST\t", "\tST "))
-                  .map(line -> line.replaceAll(" STE\t", " STE "))
-                  .map(line -> line.replaceAll("\tSTE\t", "\tSTE "))
-                  .map(line -> line.replaceAll("SAINT\t", "SAINT "))
-                  .map(line -> line.replaceAll("\tSUR\t", " SUR "))
-                  .map(line -> line.replaceAll("\tLA\t", "\tLA "))
-                  .map(line -> line.replaceAll(" LA\t", " LA "))
-                  .map(line -> line.replaceAll("^LA\t", "LA "))
-                  .map(line -> line.replaceAll("\tDE\t", " DE "))
-                  .map(line -> line.replaceAll("\tEN\t", " EN "))
-                  .map(line -> line.replaceAll("\tNORD", " NORD"))
-                  .map(line -> line.replaceAll("\tSUD", " SUD"))
-                  .map(line -> line.replaceAll("\tOUEST", " OUEST"))
-                  .map(line -> line.replaceAll("\tEST", " EST"))
-                  .map(line -> line.replaceAll("L\tISLE", "L ISLE"))
-                  .map(line -> line.replaceAll("DOLE LA", "DOLE\tLA"))
-                  .map(line -> line.replaceAll("\tS/", " S/"))
-                  .map(line -> line.replaceAll("S/\t", "S/ "))
-                  .map(line -> line.replaceAll("GONDREVILLE\tA77", "GONDREVILLE A77"))
-                  .map(line -> line.replaceAll("VOIE\tSACREE", "VOIE SACREE"))
-                  .map(line -> line.replaceAll("VULCANIA\tBROMONT", "VULCANIA BROMONT"))
-                  .map(line -> line.replaceAll("ST ETIENNE\tAU\tTEMPLE", "ST ETIENNE AU TEMPLE"))
-                  .map(line -> line.replaceAll("TIL\tCHATEL", "TIL CHATEL"))
-                  .map(line -> line.replaceAll("ST JULIEN\tSANCY", "ST JULIEN SANCY"))
-                  .map(line -> line.replaceAll("\tD\t", " D "))
-                  .map(line -> line.replaceAll("\tSOUS\t", " SOUS "))
-                  .map(line -> line.replaceAll("\tAUX\t", " AUX "))
-                  .map(line -> line.replaceAll("\tLES\t", "\tLES "))
-                  .map(line -> line.replaceAll("MACON\tCENTRE", "MACON CENTRE"))
-                  .map(line -> line.replaceAll("CRIMOLOIS ", "CRIMOLOIS\t"))
-                  .map(line -> line.replaceAll("LE\tMIROIR", "LE MIROIR"))
-                  .map(line -> line.replaceAll("DIJON SUD ", "DIJON SUD\t"))
-                  .map(line -> line.replaceAll("CHALONS\t", "CHALONS "))
-                  .map(line -> line.replaceAll("\t-\t", " - "))
-                  .map(line -> line.replaceAll("AMBOISE\tCH.RENAULT", "AMBOISE CH.RENAULT"))
-                  .map(line -> line.replaceAll("MONT\tCHOISY", "MONT CHOISY"))
-                  .map(line -> line.replaceAll("MONT\tCHOISY", "MONT CHOISY"))
-                  .map(line -> line.replaceAll("ST GERMAIN\tLES VERGNE", "ST GERMAIN LES VERGNE"))
-                  .map(line -> line.replaceAll("Système\tOuvert", "Système Ouvert"))
-                  .map(line -> line.replaceAll("STE\tMENEHOULD", "STE MENEHOULD"))
-                  .map(line -> line.replaceAll("ST\tROMAIN SUR CHER", "ST ROMAIN SUR CHER"))
-                  .map(line -> line.replaceAll("ST\tMARTIN", "ST MARTIN"))
-                  .map(line -> line.replaceAll("ST\tJULIEN\tSANCY", "ST JULIEN SANCY"))
-                  .map(line -> line.replaceAll("ST\tHILAIRE", "ST HILAIRE"))
-                  .map(line -> line.replaceAll("ST\tGIBRIEN", "ST GIBRIEN"))
-                  .map(line -> line.replaceAll("^ST\t", "ST "))
-                  .map(line -> line.replaceAll("VIERZON-EST ", "VIERZON-EST\t"))
-                  .map(line -> line.replaceAll("USSEL OUEST ", "USSEL OUEST\t"))
-                  .map(line -> line.replaceAll("USSEL EST ", "USSEL EST\t"))
-                  .map(line -> line.replaceAll("BESANCON OUEST ", "BESANCON OUEST\t"))
-                  .map(line -> line.replaceAll("BEYNOST ", "BEYNOST\t"))
-                  .map(line -> line.replaceAll("TULLE EST ", "TULLE EST\t"))
-                  .map(line -> line.replaceAll("LES\tEPRUNES", "LES EPRUNES"))
-                  .map(line -> line.replaceAll("LA\tCOTIERE ", "LA COTIERE\t"))
-                  .map(line -> line.replaceAll("LA\tBOISSE ", "LA BOISSE\t"))
-                  .map(line -> line.replaceAll("GERMAIN\tLES VERGNE", "GERMAIN LES VERGNE"))
-                  .map(line -> line.replaceAll("ETIENNE\tAU\tTEMPLE", "ETIENNE AU TEMPLE"))
-                  .map(line -> line.replaceAll("LE\tTOURNEAU", "LE TOURNEAU"))
-                  .map(line -> line.replaceAll("QUINCIEUX\tBARRIERE\tSystème Ouvert", "QUINCIEUX BARRIERE\tSystème Ouvert"))
-                  .map(line -> line.replaceAll("REIMS NORD (péage d'Ormes)", "REIMS NORD (ORMES)"))
-                  .map(line -> line.replaceAll("REIMS OUEST (péage de Thillois)", "REIMS NORD (THILLOIS)"))
-                  .map(line -> COMPILE.matcher(line).replaceAll(matchResult -> "\t" + matchResult.group(1)))
-                  .forEach(x -> {
-                      String[] split = x.split("\t");
-                      if (split.length != 8) {
-                          errors.incrementAndGet();
-                          System.err.println(split.length + " " + x);
-                      }
-                      writer.println(x);
-                  });
-            System.out.println(errors);
+        var triangleBuilder = new TriangleBuilder(triangleOutputPath);
+        extractMatrix(pdf, Category.Car, triangleBuilder, 4);
+        extractMatrix(pdf, Category.Motorcycle, triangleBuilder, 4);
+    }
+
+    private void extractMatrix(Path pdf, Category category, TriangleBuilder triangleBuilder, int page) throws IOException {
+        var lines = extractText(pdf, page);
+        var output = Path.of(pdf.getParent().toString(), "out");
+        var triangleOutputPath = Path.of(output.toString(), "triangle");
+        var outputPath = Path.of(triangleOutputPath.toString(), category.name());
+        if (!Files.exists(outputPath)) {
+            Files.createDirectories(outputPath);
         }
+        if (category == Category.Car) {
+            writeFile(outputPath, "Sanef-A16-A29", lines, 1, 23);
+            writeFile(outputPath, "Sanef-A1-A2-A26-NORD-A29", lines, 24, 57);
+            writeFile(outputPath, "Sanef-A4-A26-SUD", lines, 59, 96);
+        } else {
+            writeFile(outputPath, "Sanef-A16-A29", lines, 1, 23);
+            writeFile(outputPath, "Sanef-A1-A2-A26-NORD-A29", lines, 24, 57);
+            writeFile(outputPath, "Sanef-A4-A26-SUD", lines, 59, 96);
+        }
+    }
+
+    private void writeFile(Path outputPath, String filename, String[] lines, int startLine, int endLine) {
+        var currentYear = LocalDate.now().getYear();
+        Path path = Path.of(outputPath.toString(), currentYear + "_" + filename + ".tsv");
+        try (var writer = Files.newBufferedWriter(path)) {
+            for (var i = startLine; i < endLine; i++) {
+                writer.write(lines[i]);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String[] extractText(Path pdf, int page) throws IOException {
+        return PDFExtractor.extractText(pdf.toFile(), page, page)
+                           .replaceAll("\r", "")
+                           .replaceAll(" N°\\d+", "")
+                           .replaceAll("(\\d+) (\\d+)", "$1\t$2")
+                           .replaceAll("(\\d+) (\\w)", "$1\t$2")
+                           .split("\n");
     }
 }
